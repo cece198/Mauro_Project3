a56bf412c1fcf5a8781fe523f07cf46f4c926035
Cecelia <cece198@gmail.com>
2019-05-16 10:03:22 -0400

    When second button clicked, return centimeters to inches

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 $(document).ready(function(){
     $('#submitI').on('click', function(){
         var apiBase = 'http://statenweb.mockable.io/conversions/';
-        var input1 = $('#Centimeters').val();
-        
-        //console.log("Testing 123");
+        var input1 = $('#Inches').val();
+
         axios.get(apiBase).then(function(response){
-            //console.log(response);
+            console.log(response);
             var num = response.data.centimetersInInch;
-            //console.log(num);
-            //alert(num*2);
-            var output1 = inputI * num;
-            $('answerI').html(output1);
+            var output1 = input1 * num;
+            $('#answerI').html(output1);
         });
+    });
+    $('#submitC').on('click', function(){
+        var apiBase = 'http://statenweb.mockable.io/conversions/';
+        var input2 = $('#Centimeters').val();
+
+        axios.get(apiBase).then(function(response){
+            var num2 = response.data.inchesInCm;
+            var output2 = input2 * num2;
+            $('#answerC').html(output2);
+        });
+
     });
 });
